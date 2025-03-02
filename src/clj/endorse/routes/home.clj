17c9bdd3ebb247f9ade7fc79be11092a69be3ee9
Patrick Bucher <patrick.bucher@mailbox.org>
2025-03-02 12:52:00 +0100

    validation

--- src/clj/endorse/routes/home.clj
+++ src/clj/endorse/routes/home.clj
@@ -5,14 +5,36 @@
    [clojure.java.io :as io]
    [endorse.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:endorsements (db/get-endorsements)}))
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render
+   request
+   "home.html"
+   (merge {:endorsements (db/get-endorsements)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn save-endorsement! [{:keys [params]}]
-  (db/save-endorsement! params)
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-endorsement! params)
+      (response/found "/"))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
